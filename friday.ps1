65205cff879558b9c323e3c1816c22e78398054d
Ryan Strange <126738863+RyanStrange@users.noreply.github.com>
2024-05-30 09:39:23 -0700

    Create friday.ps1

---
+++ friday.ps1
@@ -0,0 +1,256 @@
+#student names, usernames, and passwords need to be filled in
+#intended to run on one of our groups home desktops
+
+start-process https://camosun.libcal.com/space/24161
+sleep 10
+[system.windows.forms.sendkeys]::sendwait('{ESC}')
+sleep 2
+[system.windows.forms.sendkeys]::sendwait('{TAB 9}')
+sleep 2
+[system.windows.forms.sendkeys]::sendwait('{ENTER 7}')
+sleep 2
+[system.windows.forms.sendkeys]::sendwait('{TAB 4}')
+sleep 2
+[system.windows.forms.sendkeys]::sendwait('{ENTER}')
+sleep 2
+[system.windows.forms.sendkeys]::sendwait('{TAB}')
+sleep 2
+[system.windows.forms.sendkeys]::sendwait('{ENTER}')
+sleep 2
+[system.windows.forms.sendkeys]::sendwait('{DOWN 3}')
+sleep 2
+[system.windows.forms.sendkeys]::sendwait('{ENTER}')
+sleep 2
+[system.windows.forms.sendkeys]::sendwait('{TAB 3}')
+sleep 2
+[system.windows.forms.sendkeys]::sendwait('{ENTER}')
+sleep 10
+[system.windows.forms.sendkeys]::sendwait('{TAB 4}')
+sleep 2
+[system.windows.forms.sendkeys]::sendwait("<Student 1 number>")
+sleep 2
+[system.windows.forms.sendkeys]::sendwait('{TAB}')
+sleep 2
+[system.windows.forms.sendkeys]::sendwait("<Student 1 password>")
+sleep 2
+[system.windows.forms.sendkeys]::sendwait('{TAB 3}')
+sleep 2
+[system.windows.forms.sendkeys]::sendwait('{ENTER}')
+sleep 2
+[system.windows.forms.sendkeys]::sendwait('{TAB 8}')
+sleep 2
+[system.windows.forms.sendkeys]::sendwait("<Student 1 first name>")
+sleep 2
+[system.windows.forms.sendkeys]::sendwait('{TAB}')
+sleep 2
+[system.windows.forms.sendkeys]::sendwait("<student 1 last name>")
+sleep 2
+[system.windows.forms.sendkeys]::sendwait('{TAB}')
+sleep 2
+[system.windows.forms.sendkeys]::sendwait('{ENTER}')
+sleep 10
+[system.windows.forms.sendkeys]::sendwait('{TAB 2}')
+sleep 2
+[system.windows.forms.sendkeys]::sendwait('{ENTER}')
+sleep 10
+start-process https://camosun.libcal.com/space/24161
+sleep 10
+[system.windows.forms.sendkeys]::sendwait('{ESC}')
+sleep 2
+[system.windows.forms.sendkeys]::sendwait('{TAB 9}')
+sleep 2
+[system.windows.forms.sendkeys]::sendwait('{ENTER 7}')
+sleep 2
+[system.windows.forms.sendkeys]::sendwait('{TAB 8}')
+sleep 2
+[system.windows.forms.sendkeys]::sendwait('{ENTER}')
+sleep 2
+[system.windows.forms.sendkeys]::sendwait('{TAB}')
+sleep 2
+[system.windows.forms.sendkeys]::sendwait('{ENTER}')
+sleep 2
+[system.windows.forms.sendkeys]::sendwait('{DOWN 3}')
+sleep 2
+[system.windows.forms.sendkeys]::sendwait('{ENTER}')
+sleep 2
+[system.windows.forms.sendkeys]::sendwait('{TAB 3}')
+sleep 2
+[system.windows.forms.sendkeys]::sendwait('{ENTER}')
+sleep 10
+[system.windows.forms.sendkeys]::sendwait('{TAB 4}')
+sleep 2
+[system.windows.forms.sendkeys]::sendwait("<Student 2 number>")
+sleep 2
+[system.windows.forms.sendkeys]::sendwait('{TAB}')
+sleep 2
+[system.windows.forms.sendkeys]::sendwait("<Student 2 password>")
+sleep 2
+[system.windows.forms.sendkeys]::sendwait('{TAB 3}')
+sleep 2
+[system.windows.forms.sendkeys]::sendwait('{ENTER}')
+sleep 10
+[system.windows.forms.sendkeys]::sendwait('{TAB 8}')
+sleep 2
+[system.windows.forms.sendkeys]::sendwait("<Student 2 first name>")
+sleep 2
+[system.windows.forms.sendkeys]::sendwait('{TAB}')
+sleep 2
+[system.windows.forms.sendkeys]::sendwait("<Student 2 last name>")
+sleep 2
+[system.windows.forms.sendkeys]::sendwait('{TAB}')
+sleep 2
+[system.windows.forms.sendkeys]::sendwait('{ENTER}')
+sleep 10
+[system.windows.forms.sendkeys]::sendwait('{TAB 2}')
+sleep 2
+[system.windows.forms.sendkeys]::sendwait('{ENTER}')
+sleep 10
+start-process https://camosun.libcal.com/space/24161
+sleep 10
+[system.windows.forms.sendkeys]::sendwait('{ESC}')
+sleep 2
+[system.windows.forms.sendkeys]::sendwait('{TAB 9}')
+sleep 2
+[system.windows.forms.sendkeys]::sendwait('{ENTER 7}')
+sleep 2
+[system.windows.forms.sendkeys]::sendwait('{TAB 16}')
+sleep 2
+[system.windows.forms.sendkeys]::sendwait('{ENTER}')
+sleep 2
+[system.windows.forms.sendkeys]::sendwait('{TAB}')
+sleep 2
+[system.windows.forms.sendkeys]::sendwait('{ENTER}')
+sleep 2
+[system.windows.forms.sendkeys]::sendwait('{DOWN 2}')
+sleep 2
+[system.windows.forms.sendkeys]::sendwait('{ENTER}')
+sleep 2
+[system.windows.forms.sendkeys]::sendwait('{TAB 3}')
+sleep 2
+[system.windows.forms.sendkeys]::sendwait('{ENTER}')
+sleep 10
+[system.windows.forms.sendkeys]::sendwait('{TAB 4}')
+sleep 2
+[system.windows.forms.sendkeys]::sendwait("<Student 3 number>")
+sleep 2
+[system.windows.forms.sendkeys]::sendwait('{TAB}')
+sleep 2
+[system.windows.forms.sendkeys]::sendwait("<Student 3 password>")
+sleep 2
+[system.windows.forms.sendkeys]::sendwait('{TAB 3}')
+sleep 2
+[system.windows.forms.sendkeys]::sendwait('{ENTER}')
+sleep 10
+[system.windows.forms.sendkeys]::sendwait('{TAB 8}')
+sleep 2
+[system.windows.forms.sendkeys]::sendwait("<Student 3 first name>")
+sleep 2
+[system.windows.forms.sendkeys]::sendwait('{TAB}')
+sleep 2
+[system.windows.forms.sendkeys]::sendwait("<Student 3 last name>")
+sleep 2
+[system.windows.forms.sendkeys]::sendwait('{TAB}')
+sleep 2
+[system.windows.forms.sendkeys]::sendwait('{ENTER}')
+sleep 10
+[system.windows.forms.sendkeys]::sendwait('{TAB 2}')
+sleep 2
+[system.windows.forms.sendkeys]::sendwait('{ENTER}')
+sleep 10
+start-process https://camosun.libcal.com/space/18517
+sleep 10
+[system.windows.forms.sendkeys]::sendwait('{ESC}')
+sleep 2
+[system.windows.forms.sendkeys]::sendwait('{TAB 9}')
+sleep 2
+[system.windows.forms.sendkeys]::sendwait('{ENTER 7}')
+sleep 2
+[system.windows.forms.sendkeys]::sendwait('{TAB 19}')
+sleep 2
+[system.windows.forms.sendkeys]::sendwait('{ENTER}')
+sleep 2
+[system.windows.forms.sendkeys]::sendwait('{TAB 3}')
+sleep 2
+[system.windows.forms.sendkeys]::sendwait('{ENTER}')
+sleep 10
+[system.windows.forms.sendkeys]::sendwait('{TAB 4}')
+sleep 2
+[system.windows.forms.sendkeys]::sendwait("<Student 3 number >")
+sleep 2
+[system.windows.forms.sendkeys]::sendwait('{TAB}')
+sleep 2
+[system.windows.forms.sendkeys]::sendwait("<Student 3 password>")
+sleep 2
+[system.windows.forms.sendkeys]::sendwait('{TAB 3}')
+sleep 2
+[system.windows.forms.sendkeys]::sendwait('{ENTER}')
+sleep 10
+[system.windows.forms.sendkeys]::sendwait('{TAB 8}')
+sleep 2
+[system.windows.forms.sendkeys]::sendwait("<Student 3 first name>")
+sleep 2
+[system.windows.forms.sendkeys]::sendwait('{TAB}')
+sleep 2
+[system.windows.forms.sendkeys]::sendwait("<Student 3 last name>")
+sleep 2
+[system.windows.forms.sendkeys]::sendwait('{TAB}')
+sleep 2
+[system.windows.forms.sendkeys]::sendwait('{ENTER}')
+sleep 10
+[system.windows.forms.sendkeys]::sendwait('{TAB 2}')
+sleep 2
+[system.windows.forms.sendkeys]::sendwait('{ENTER}')
+sleep 10
+start-process https://camosun.libcal.com/space/18517
+sleep 10
+[system.windows.forms.sendkeys]::sendwait('{ESC}')
+sleep 2
+[system.windows.forms.sendkeys]::sendwait('{TAB 9}')
+sleep 2
+[system.windows.forms.sendkeys]::sendwait('{ENTER 7}')
+sleep 2
+[system.windows.forms.sendkeys]::sendwait('{TAB 20}')
+sleep 2
+[system.windows.forms.sendkeys]::sendwait('{ENTER}')
+sleep 2
+[system.windows.forms.sendkeys]::sendwait('{TAB}')
+sleep 2
+[system.windows.forms.sendkeys]::sendwait('{ENTER}')
+sleep 2
+[system.windows.forms.sendkeys]::sendwait('{DOWN 3}')
+sleep 2
+[system.windows.forms.sendkeys]::sendwait('{ENTER}')
+sleep 2
+[system.windows.forms.sendkeys]::sendwait('{TAB 3}')
+sleep 2
+[system.windows.forms.sendkeys]::sendwait('{ENTER}')
+sleep 10
+[system.windows.forms.sendkeys]::sendwait('{TAB 4}')
+sleep 2
+[system.windows.forms.sendkeys]::sendwait("<Student 4 number>")
+sleep 2
+[system.windows.forms.sendkeys]::sendwait('{TAB}')
+sleep 2
+[system.windows.forms.sendkeys]::sendwait("<Student 4 password >")
+sleep 2
+[system.windows.forms.sendkeys]::sendwait('{TAB 3}')
+sleep 2
+[system.windows.forms.sendkeys]::sendwait('{ENTER}')
+sleep 10
+[system.windows.forms.sendkeys]::sendwait('{TAB 8}')
+sleep 2
+[system.windows.forms.sendkeys]::sendwait("<Student 4 first name>")
+sleep 2
+[system.windows.forms.sendkeys]::sendwait('{TAB}')
+sleep 2
+[system.windows.forms.sendkeys]::sendwait("<Student 4 last name>")
+sleep 2
+[system.windows.forms.sendkeys]::sendwait('{TAB}')
+sleep 2
+[system.windows.forms.sendkeys]::sendwait('{ENTER}')
+sleep 10
+[system.windows.forms.sendkeys]::sendwait('{TAB 2}')
+sleep 2
+[system.windows.forms.sendkeys]::sendwait('{ENTER}')
+sleep 10
+Stop-Process -Name Chrome -Force
